bd420024f4d33c79561001258175f3a96bba640e
igorgbr <igorgphotoarte@gmail.com>
2022-01-19 18:42:07 -0300

    pipe operator

--- anomFunc/example.exs
+++ anomFunc/example.exs
@@ -5,3 +5,10 @@ up = &String.upcase/1 #'bind' para funçoes nomeadas
 # OUTPUT
 IO.puts(up.("Hello"))
 IO.puts(sum.(1, 2))
+
+# OUTPUT com Pipe Operator
+up.("Hello") |> IO.puts
+
+"hello"
+|> String.upcase
+|> IO.puts
